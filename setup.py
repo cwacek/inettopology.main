1559c2ff95b754bf65ad168f5b80919df2b1b620
Chris Wacek <cwacek@gmail.com>
2013-09-05 17:47:41 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,26 @@
 import distribute_setup
 distribute_setup.use_setuptools()
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
-    name = "inettopology-asmap",
-    version = "0.1",
-    packages = find_packages(),
+    name="inettopology-asmap",
+    version="0.1",
+    packages=find_packages(),
 
-    install_requires = [
-                "redis",
-                "argparse"],
+    install_requires=[
+                      "redis",
+                      "argparse"
+                     ],
 
-    entry_points = {
-        'console_scripts': [
-            ]
+    entry_points={
+                  'console_scripts': [
+                    'inettopology = inettopology.asmap:cmdline'
+                  ]
     },
 
-    author = "Chris Wacek",
-    author_email = "cwacek@cs.georgetown.edu",
-    description = "Internet Topology Graph Creator",
-    license = "LGPL"
+    author="Chris Wacek",
+    author_email="cwacek@cs.georgetown.edu",
+    description="Internet Topology Graph Creator",
+    license="LGPL"
 )
